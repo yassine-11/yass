30902bb3d45ef7efcd09406bfbe4d179fe780285
yassine <sabekyassine@gmail.com>
2023-08-01 20:08:44 +0900

    last3

--- hello.py
+++ hello.py
@@ -5,7 +5,19 @@ b=5
 print(a+b)
 for n in range(0,40):
    print(n)
-fruitlist = ["apple","banana","grape"]
-for fruit in fruitlist 
+   if n % 2 == 1:
+        print("odd")
+   else:
+        print("even")     
+
+fruitlist = ["apple","banana","grape","Orange"]
+for fruit in fruitlist: 
     print(fruit)
+c = 5 == 3
+d = 7 > 3
+print(c,d)
+n=0
+while n<50:
+    print(n)
+    n+=1
     
